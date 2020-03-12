59e0808995ac5ec1570ffcb3aad28d22d9da48c9
Jon Baltz <baltz.jon.r@gmail.com>
2020-03-12 16:19:46 -0600

    Complete all tests in database.test.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/reviews', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/reviews', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
--- spec/database.test.js
+++ spec/database.test.js
@@ -1,7 +1,37 @@
 const Review = require('../database/Review.js');
+const mongoose = require('mongoose');
 
-test('the seed script adds to the database', () => {
-  Review.find({ item: 1 }, (err, items) => {
-    expect(item.length).toBe(3);
+describe('mongoDB database', () => {
+
+  test('the seed script adds to the database', done => {
+    // The seed file has 3 reviews for item 1
+    // Needs to be changed if more data is added
+    Review.find({ item: 1 }, (err, items) => {
+      expect(items.length).toBe(3);
+      done();
+    });
   });
+
+  test('data.json is a json object', done => {
+    const data = require('../database/data.json');
+    expect(typeof data).toBe('object');
+    done();
+  });
+
+  test('the schema should have the correct items', done => {
+    Review.findOne({ item: 1 }, (err, item) => {
+      expect(item).toEqual(
+        expect.objectContaining({
+          author: expect.any(String),
+          body: expect.any(String),
+          item: expect.any(Number),
+          rating: expect.any(Number),
+          likes: expect.any(Number)
+        })
+      );
+      mongoose.connection.close();
+      done();
+    });
+  });
+
 });
