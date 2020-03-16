9ae1a9caa1b380abd944ff0efc4b1574cf6e6805
Jon Baltz <baltz.jon.r@gmail.com>
2020-03-16 11:04:41 -0600

    Add User icon of colored circle with first char

--- client/src/components/reviewItem.jsx
+++ client/src/components/reviewItem.jsx
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 
 const ReviewItem = function(props) {
   const review = props.review;
+
+  // Creates a string for rating withstars instead of numbers
+
   return (
     <div className="review-item">
       <div className="row">
-        <div className="col-sm-8">
-          <p className="review-item-author">{review.author}</p>
-          <p className="review-item-rating">{review.rating}</p>
+        <div className="col-sm-2 author-image">
+          <p className="circle">{review.author[0]}</p>
         </div>
-        <p className="review-item-likes col-sm-4">Likes: {review.likes}</p>
+
+        <div className="col-sm-10">
+          <div className="row">
+            <div className="col-sm-8">
+              <p className="review-item-author">{review.author}</p>
+              <p className="review-item-rating">{review.rating}</p>
+            </div>
+            <p className="review-item-likes col-sm-4">Likes: {review.likes}</p>
+          </div>
+          <p className="review-item-body">{review.body}</p>
+        </div>
       </div>
-      <p className="review-item-body">{review.body}</p>
     </div>
   );
 };
