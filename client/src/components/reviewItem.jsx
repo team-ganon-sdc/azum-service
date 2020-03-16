cb2a38df8287dfe74a7448b27805bd2fd2213e0b
Jon Baltz <baltz.jon.r@gmail.com>
2020-03-16 13:22:11 -0600

    Move the like total so it looks less akward

--- client/src/components/reviewItem.jsx
+++ client/src/components/reviewItem.jsx
@@ -3,20 +3,14 @@ import React, { Component } from 'react';
 const ReviewItem = function(props) {
   const review = props.review;
 
-  // Creates a string for rating withstars instead of numbers
   const stars = [];
-  // for (let i = 0; i < review.rating; i++) {
-  //   stars.push(<i key={i} className="icon-star"></i>);
-  // }
   if (review.rating % 2) {
-    // if odd
     let numbStars = (review.rating - 1) / 2;
     for (let i = 0; i < numbStars; i++) {
       stars.push(<i key={i} className="icon-star"></i>);
     }
     stars.push(<i key="half-empty" className="icon-star-half-empty"></i>);
   } else {
-    // if even
     let numbStars = (review.rating - 1) / 2;
     for (let i = 0; i < numbStars; i++) {
       stars.push(<i key={i} className="icon-star"></i>);
@@ -26,17 +20,18 @@ const ReviewItem = function(props) {
   return (
     <div className="review-item">
       <div className="row">
-        <div className="col-sm-2 author-image">
+        {/* <div className="col-sm-1"></div> */}
+        <div className="col-sm-1 author-image">
           <p className="circle">{review.author[0]}</p>
         </div>
 
-        <div className="col-sm-10">
+        <div className="col-sm-11">
           <div className="row">
-            <div className="col-sm-8">
+            <div className="col-sm-10">
               <p className="review-item-author">{review.author}</p>
               <p className="review-item-rating">{stars}</p>
             </div>
-            <p className="review-item-likes col-sm-4">Likes: {review.likes}</p>
+            <p className="review-item-likes col-sm-2">Likes: {review.likes}</p>
           </div>
           <p className="review-item-body">{review.body}</p>
         </div>
