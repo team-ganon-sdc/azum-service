8cd1cd53cd54696c5cd79ccde1a9bc95259fafb6
Jon Baltz <baltz.jon.r@gmail.com>
2020-03-18 13:01:33 -0600

    Add test for React that ensures a ReviewList is rendered

--- spec/App.test.js
+++ spec/App.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 
-import App, { Review } from '../client/src/index.jsx';
+import Review from '../client/src/components/app.jsx';
+import ReviewList from '../client/src/components/reviewList.jsx';
 
 describe('Review', () => {
-  test('snapshot renders', () => {
-    const component = renderer.create(<Review />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  test('renders review list', () => {
+    const wrapper = shallow(<Review />);
+    expect(wrapper.find(ReviewList).length).toEqual(1);
   });
 });
