eef76f603a045e11e24f42e4c7788bca4c0f5046
Azum Beg <azumbeg@gmail.com>
2020-04-01 18:36:14 -0500

    Write data to JSON file

--- database/generateData.js
+++ database/generateData.js
@@ -12,30 +12,31 @@ const faker = require("faker");
 
 const data = [];
 
-const i = 10000000
+let i = 10
 while (i--) {
-  const reviewCount = Math.floor(Math.random() * 16 + 5); // generate random # of reviews between 5 and 20
+  let reviewCount = Math.floor(Math.random() * 16) + 5; // generate random # of reviews between 5 and 20
 
   while (reviewCount--) {
     data.push({
-      author: ,
+      author: faker.name.firstName(),
 
-      body: ,
+      body: faker.lorem.paragraph(),
       // item id
-      item: i,
+      item: i + 1,
       // generate rating between 1 and 10
-      rating: Math.floor(Math.random() * 10 + 1),
-      likes:
+      rating: Math.floor(Math.random() * 10) + 1,
+      likes: Math.floor(Math.random() * 490 + 1) + 10
     });
   }
 }
 
 const jsonData = JSON.stringify(data);
+// console.log(jsonData)
 
-// fs.writeFile("data.json", jsonData, err => {
-//   if (err) {
-//     console.log(err);
-//   }
-// });
+fs.writeFile("database/data2.json", jsonData, err => {
+  if (err) {
+    console.log(err);
+  }
+});
 
-module.exports = data;
+// module.exports = data;
