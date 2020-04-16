235b525cb670a619e42875d2af2f560b4993bca2
Azum Beg <azumbeg@gmail.com>
2020-04-15 19:03:07 -0500

    Implement clustering

--- server/index.js
+++ server/index.js
@@ -48,6 +48,12 @@ if (cluster.isMaster) {
   const app = express();
   const port = 3002;
 
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
+
   app.use(express.static(__dirname + '/../client/dist'));
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
